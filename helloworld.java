36401defb3f8e633318e248d891a1dc277bdef3e
Satyam Gajjar <66769510+satyamgajjar21@users.noreply.github.com>
2020-08-06 22:36:03 +0530

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,13 @@
+package samplehelp;
+
+public class Samplehelp
+{
+    public static void main(String[] args)
+    {    
+        System.out.println("Hello World");
+    }
+    
+}
+
+//output
+//Hello World
